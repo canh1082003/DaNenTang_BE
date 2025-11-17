bdd16ef71e63d97ef36a80b4c883e754791c431a
Nguyen Cong Anh <nguyenconganh@Mac.lan>
2025-11-17 14:01:14 +0700

    fix version 2 video call 2 user

--- src/modules/WebRTC/index.ts
+++ src/modules/WebRTC/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import WebRtcController from "./WebRtcController";
+import verifyTokenMiddleware from "@/middlewares/chat.middlewares";
 
 export const WebRtcRouter = Router();
-WebRtcRouter.get("/call",WebRtcController.getIceServers);
+WebRtcRouter.get("/call",verifyTokenMiddleware, WebRtcController.getIceServers);
